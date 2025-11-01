f1adfbff08dbbea9d4cbc6ace70381c9e514f1a8
Seu Nome <thiagostopassola@gmail.com>
2025-10-31 21:52:25 -0300

    att

--- src/components/structure/header/header.js
+++ src/components/structure/header/header.js
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="logo">🌐 Mini Ecommerce do Senac</h1>
+      <h1 className="logo"> Senac</h1>
 
       <nav className="nav">
         <NavButton to="/">Home</NavButton>
