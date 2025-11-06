83f7ceb269f97fa14030ec57582f954694f8fe39
Seu Nome <thiagostopassola@gmail.com>
2025-11-06 20:12:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'; // Importa o CSS
 import Header from './components/structure/header/Header';
 import Main from './components/structure/main_/Main'
 import Footer from './components/structure/footer/Footer'
+import { AuthProvider } from "./context/AuthContext";
+
 
 function App() {
   return (
